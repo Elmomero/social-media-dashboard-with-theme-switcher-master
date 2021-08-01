0aede5bbfd7ac2c0f76cd0deb55a2f1dc459442d
Leandro Arguello <leandro.arguello.99@gmail.com>
2021-08-01 16:59:50 -0500

    si x2

--- js/code.js
+++ js/code.js
@@ -8,21 +8,17 @@ btnDark.addEventListener('click', ()=>{
 
     if(btnDark.classList.contains('active')){
         localStorage.setItem('dark-mode', 'true');
-        txtDark.textContent = 'Dark Mode';
     }
     else{
         localStorage.setItem('dark-mode','false')
-        txtDark.textContent = 'Light Mode'
     }    
 });
 
 if(localStorage.getItem('dark-mode') === 'true'){
     document.body.classList.add('dark');
     btnDark.classList.add('active');
-    txtDark.textContent = 'Dark Mode';
 }else{
     document.body.classList.remove('dark');
     btnDark.classList.remove('active');
-    txtDark.textContent = 'Light Mode';
 
 }
